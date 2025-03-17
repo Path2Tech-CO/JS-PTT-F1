c38edf8a2dbe2841622938a5ef41b10014e18df5
Cathiana <Cathianao@gmail.com>
2025-03-17 01:27:11 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const todos = [
+  {
+    title: "Apply for Tech aligned roles",
+    description: "This is the 1st initial todo",
+    complete: false,
+  }
+];
+
+function addTodo(title, description) {
+  const newTodo = {
+    title: title, 
+    description: description,
+    complete: false
+  };
+  todos.push(newTodo);
+}
+
+function removeTodo(){}
+function editTodo(){}
+function markTodoComplete(){}
+function displayTodoLength(){}
+
+function app(){
+  console.log('Welcome to the Todo Application');
+  console.log('================================');
+  // You will need to call your methods below this comment to edit the todos array
+
+  // You will need to call your methods above this comment to edit the todos array
+  console.log('Here is a list of your todos:');
+  // Print the length of the todos array below this comment
+
+  // Print the length of the todos array above this comment
+  // Iterare over the todos array and console.log each todo below this comment
+  console.log(todos);
+}
+
+app();
